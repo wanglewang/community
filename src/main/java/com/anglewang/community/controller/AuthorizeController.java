13e7a76163c3b57cb4256bff664c694556df8444
AngleWang <1833515432@qq.com>
2020-02-14 23:50:46 +0800

    使用配置文件替代网站地址clientId等

--- src/main/java/com/anglewang/community/controller/AuthorizeController.java
+++ src/main/java/com/anglewang/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.anglewang.community.dto.AccessTokenDTO;
 import com.anglewang.community.dto.GitHubUser;
 import com.anglewang.community.provider.GitHubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +15,12 @@ import java.io.IOException;
 public class AuthorizeController {
     @Autowired
     private GitHubProvider gitHubProvider;
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
 
     @GetMapping("/callback")
     /**
@@ -22,10 +29,10 @@ public class AuthorizeController {
     public String callback(@RequestParam(name = "code")String code,
                            @RequestParam(name = "state")String state) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("b85fd778f06b1317767d");
-        accessTokenDTO.setClient_secret("4c838cd75f26966e84ff535607e3e8eacf4ff474");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
         GitHubUser user = gitHubProvider.getUser(accessToken);
